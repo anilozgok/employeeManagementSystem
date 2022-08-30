a9fe462e6680a0ac6074b66aa8649ef65f86023c
Anil Can OZGOK <ozgok1903@gmail.com>
2022-08-31 00:44:50 +0300

    Minor fixes

--- src/main/java/org/AnilCan/employeeManagement/Service/IDepartmentService.java
+++ src/main/java/org/AnilCan/employeeManagement/Service/IDepartmentService.java
@@ -7,8 +7,13 @@ import java.util.List;
 public interface IDepartmentService {
 
     List<department> getAllData();
+
     void add(department dep);
+
     void update(department dep);
+
     void delete(department dep);
+
     department getByID(int id);
+    
 }
--- src/main/java/org/AnilCan/employeeManagement/Service/IEmployeesService.java
+++ src/main/java/org/AnilCan/employeeManagement/Service/IEmployeesService.java
@@ -5,9 +5,15 @@ import org.AnilCan.employeeManagement.Entities.employees;
 import java.util.List;
 
 public interface IEmployeesService {
+
     List<employees> getAllData();
+
     void add(employees emp);
+
     void update(employees emp);
+
     void delete(employees emp);
+
     employees getByID(int id);
+    
 }
